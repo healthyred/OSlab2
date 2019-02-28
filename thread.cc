bfbb40082dee416e4280bec15442f6ed5cf6b5d1
Zhenye Lin <19zl3@lohani.cs.williams.edu>
2019-02-28 14:52:14 -0500

    start

---
+++ thread.cc
@@ -0,0 +1,34 @@
+#include <ucontext.h>
+#include <queue>
+#include "thread.h"
+#include "interrupt.h"
+#include <tuple>
+using namespace std;
+
+static queue<ucontext_t *> readyQueue;
+static queue<tuple<ucontext_t *, int>> wait;
+//lock queue
+static ucontext_t * running;
+
+int thread_libinit(thread_startfunc_t func, void*arg)
+{
+  ucontext_t * ucontext_ptr = new ucontext_t *;
+  getcontext(ucontext_ptr);
+  char *stack = new char [STACK_SIZE];
+  ucontext_ptr->uc_stack.ss_sp = stack;
+  ucontext_ptr->uc_stack.ss_size = STACK_SIZE;
+  ucontext_ptr->uc_stack.ss_flags =0;
+  ucontext_ptr->uc_link = NULL;
+  makecontext(ucontext_ptr, (void (*)()) start, 2, arg1,arg2);
+}
+int thread_create(thread_startfunc_t func, void*arg)
+{
+
+}
+int thread_yield(void){}
+int thread_lock(unsigned int lock){}
+int thread_unlock(unsigned int lock){}
+int thread_wait(unsigned int lock, unsigned int cond){}
+int thread_signal(unsigned int lock, unsigned int cond){}
+int thread_broadcast(unsigned int lock, unsigned int cond){}
+
