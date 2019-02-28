b480221c4ddbe6efbfc14b422d7c3202acaeca37
Jian H. Lu <19jhl3@lohani.cs.williams.edu>
2019-02-28 16:40:12 -0500

    added nothing

--- thread.cc
+++ thread.cc
@@ -1,14 +1,15 @@
 #include <ucontext.h>
 #include <queue>
+#include <vector>
 #include "thread.h"
 #include "interrupt.h"
 #include <tuple>
 using namespace std;
 
-static queue<ucontext_t *> readyQueue;
-static queue<tuple<ucontext_t *, int>> wait;
+static vector<ucontext_t *> readyQueue;
+static vector<tuple<ucontext_t *, int>> wait;
 //lock queue
-static ucontext_t * running;
+static ucontext_t* running;
 
 int thread_libinit(thread_startfunc_t func, void*arg)
 {
@@ -21,11 +22,16 @@ int thread_libinit(thread_startfunc_t func, void*arg)
   ucontext_ptr->uc_link = NULL;
   makecontext(ucontext_ptr, (void (*)()) start, 2, arg1,arg2);
 }
+
 int thread_create(thread_startfunc_t func, void*arg)
 {
 
 }
-int thread_yield(void){}
+
+int thread_yield(void){
+
+}
+
 int thread_lock(unsigned int lock){}
 int thread_unlock(unsigned int lock){}
 int thread_wait(unsigned int lock, unsigned int cond){}
