e18d84b0c0e89f684d3c1f0b3788df7cb8041eb4
Jian H. Lu <19jhl3@lohani.cs.williams.edu>
2019-03-01 00:16:13 -0500

    start of threads

--- thread.cc
+++ thread.cc
@@ -4,32 +4,58 @@
 #include "thread.h"
 #include "interrupt.h"
 #include <tuple>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
 using namespace std;
 
 static vector<ucontext_t *> readyQueue;
 static vector<tuple<ucontext_t *, int>> wait;
-//lock queue
-static ucontext_t* running;
+static vector<tuple<ucontext_t *, int>> lock;
+ucontext_t* running;
 
 void ending_output(){
   cout << "Thread library exiting." << endl;
 }
 
-int thread_libinit(thread_startfunc_t func, void*arg)
+static void start(thread_startfunc_t func, void *arg){
+  func(arg);
+}
+
+int thread_libinit(thread_startfunc_t func, void * arg)
+//We create a new thread and give access from default thread to this new thread
 {
-  ucontext_t * ucontext_ptr = new ucontext_t *;
+  
+  ucontext_t * ucontext_ptr;
   getcontext(ucontext_ptr);
   char *stack = new char [STACK_SIZE];
   ucontext_ptr->uc_stack.ss_sp = stack;
   ucontext_ptr->uc_stack.ss_size = STACK_SIZE;
   ucontext_ptr->uc_stack.ss_flags =0;
   ucontext_ptr->uc_link = NULL;
-  makecontext(ucontext_ptr, (void (*)()) start, 2, arg1,arg2);
+  makecontext(ucontext_ptr, (void (*)()) start, 2, func, arg);
+  swapcontext(NULL, ucontext_ptr);
+  
+  //running = ucontext_prt;
+
 }
 
 int thread_create(thread_startfunc_t func, void*arg)
+  
 {
-
+  ucontext_t *ucontext_ptr;
+  getcontext(ucontext_ptr);
+  char *stack = new char [STACK_SIZE];
+  ucontext_ptr->uc_stack.ss_sp = stack;
+  ucontext_ptr->uc_stack.ss_size = STACK_SIZE;
+  ucontext_ptr->uc_stack.ss_flags = 0;
+  ucontext_ptr->uc_link = NULL;
+  
+  makecontext(ucontext_ptr, (void (*)()) start, 2, func, arg);
+  
+  //TODO: making the thread wait for calls, and only when we decide to signal
+  readyQueue.push_back(ucontext_ptr);
 }
 
 int thread_yield(void){
